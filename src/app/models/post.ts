e10333fb2769014c4cb214bebb0a41c42458938c
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-03-20 18:43:01 -0400

    chore: remove v1 files

--- src/app/models/post.ts
+++
@@ -1,7 +0,0 @@
-export interface Post {
-  _id: string,
-  title: string,
-  author: string,
-  body: string,
-  date: Date,
-}
--- src/app/models/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  "_id": string,
-  "username": string,
-  "email": string,
-  "password": string,
-  "createdAt": Date,
-  "updatedAt": Date,
-}
