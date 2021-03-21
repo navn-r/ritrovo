39ee775177e824c2e4b52bd40737a1cf42896283
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-03-21 12:20:36 -0400

    feat(dashboard): create empty menu component

---
+++ components/menu/Menu.tsx
@@ -0,0 +1,11 @@
+import styles from './Menu.module.css';
+
+const Menu: React.FC = () => {
+  return (
+    <div className={styles.header}>
+      <h1>Ritrovo</h1>
+    </div>
+  );
+}
+
+export default Menu;
